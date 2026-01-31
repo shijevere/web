9f3a48ca8e76088ab404d10e22e51fcbcf25e1ee
Dorian Musaj <dorianmusaj@gmail.com>
2026-01-31 19:53:04 +0100

    update confir

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  output: 'export',
+  basePath: '',
+  images: { unoptimized: true }
+}
 
 export default nextConfig;
