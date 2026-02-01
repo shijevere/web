0afa1894bbef5fdb18ecaa3e0678ec9513c41179
Dorian Musaj <dorianmusaj@gmail.com>
2026-02-01 21:44:21 +0100

    update seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -185,17 +185,17 @@ export default function RootLayout({
         <link
           rel="icon"
           type="image/png"
-          href="%PUBLIC_URL%/favicon-96x96.png"
+          href="/favicon-96x96.png"
           sizes="96x96"
         />
-        <link rel="icon" type="image/svg+xml" href="%PUBLIC_URL%/favicon.svg" />
-        <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="%PUBLIC_URL%/apple-touch-icon.png"
+          href="/apple-touch-icon.png"
         />
-        <link rel="manifest" href="%PUBLIC_URL%/site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest" />
 
         {/* Geo targeting for Albania */}
         <meta name="geo.region" content="AL" />
